eb9603a917d0735493a5667cb55916a433259da4
rongrong77 <r11528025@g.ntu.edu.tw>
2024-02-26 14:42:48 +0800

    Initial project commit

--- Test_SS.sh
+++
@@ -1,6 +0,0 @@
-python Span_Selection_copy.py \
---model_name_or_path="./SS_rol/" \
---do_predict \
---test_file="./test_for_QA.json" \
---context_file="data/context.json" \
---output_file="./pred.csv" \
